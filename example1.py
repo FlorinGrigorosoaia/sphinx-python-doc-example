2058da4c5a2584caf80a626da5ee3e6aff007ec9
Florin Grigorosoaia <floringrigorosoaia@gmail.com>
2019-11-19 16:42:21 +0200

    second

---
+++ example1.py
@@ -0,0 +1,20 @@
+def func1(arg1, arg2):
+    """
+    This function takes two arguments, etc etc...
+
+    :param arg1: some value
+    :param arg2: another value
+
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
+
+
+def func2():
+    """
+    Function with no arguments returns None
+
+    :return: None
+    
+    """
